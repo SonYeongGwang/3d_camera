691de39c7114208c6e13a1937f701063640e6cae
SonYeongGwang <syoungk20@naver.com>
2022-11-11 20:43:37 +0900

    real data from AIDIN

--- calculate_pose_error.py
+++ calculate_pose_error.py
@@ -14,22 +14,32 @@ ground_truth_pose = shape_meta_info['cam2object']
 #                                        Insert Estimated Pose                                           #
 ##########################################################################################################
 
-estimated_pose = np.array( [[ 0.01395671,  0.06192953,  0.99798294,  0.00391953],
-                            [-0.06277135,  0.99616575, -0.06093892,  0.035     ],
-                            [-0.99793034, -0.06179423,  0.0177906 ,  0.79992816],
-                            [ 0.        ,  0.        ,  0.        ,  1.        ]])
+## box
+# estimated_pose = np.array( [[-0.04023262,  0.88360023, -0.46651039, -0.04865666],
+#                             [ 0.0291515 , -0.46565166, -0.88448781,  0.01145382],
+#                             [-0.99876499, -0.04918474, -0.00702388,  0.87835401],
+#                             [ 0.        ,  0.        ,  0.        ,  1.        ]])
+
+# cylinder
+estimated_pose = np.array( [[ 0.0585681 , -0.89255204,  0.44712485, -0.04206563],
+                            [ 0.00830552,  0.44831386,  0.89383762,  0.01316391],
+                            [-0.99824886, -0.04863677,  0.03367001,  0.89330201],
+                            [ 0.        ,  0.        ,  0.        ,  1.        ]]
+                            )
 
 ##########################################################################################################
-
+# 19, 6.8
 if shape_meta_info['shape'] == 'Box':
-    mesh = o3d.geometry.TriangleMesh.create_box(width=shape_meta_info['shape_parameters']['width'], height=shape_meta_info['shape_parameters']['height'], depth=shape_meta_info['shape_parameters']['depth'])
+    mesh_ground = o3d.geometry.TriangleMesh.create_box(width=shape_meta_info['shape_parameters']['width'], height=shape_meta_info['shape_parameters']['height'], depth=shape_meta_info['shape_parameters']['depth'])
+    mesh_estimated = o3d.geometry.TriangleMesh.create_box(width=0.060154107799408774, height=0.085678466, depth=0.13982427)
 elif shape_meta_info['shape'] == 'Cylinder':
-    mesh = o3d.geometry.TriangleMesh.create_cylinder(radius=shape_meta_info['shape_parameters']['radius'], height=shape_meta_info['shape_parameters']['height'], resolution=20)
+    mesh_ground = o3d.geometry.TriangleMesh.create_cylinder(radius=shape_meta_info['shape_parameters']['radius'], height=shape_meta_info['shape_parameters']['height'], resolution=40)
+    mesh_estimated = o3d.geometry.TriangleMesh.create_cylinder(radius=0.027600782545470052, height=0.16509402879018856, resolution=40)
 elif shape_meta_info['shape'] == 'Sphere':
     mesh = o3d.geometry.TriangleMesh.create_sphere(radius=shape_meta_info['shape_parameters']['radius'], resolution=20)
 
-pcd_ground.points = mesh.vertices
-pcd_estimated.points = mesh.vertices
+pcd_ground.points = mesh_ground.vertices
+pcd_estimated.points = mesh_estimated.vertices
 
 pcd_ground.paint_uniform_color([0, 0, 1])
 pcd_estimated.paint_uniform_color([0, 1, 0])
@@ -65,7 +75,7 @@ line_sets.colors = o3d.utility.Vector3dVector(colors)
 o3d.visualization.draw_geometries([pcd_ground, pcd_estimated, line_sets])
 
 ## calculate pose error
-pose_error = (np.sum(distance_list)/point_len)*10
+pose_error = (np.sum(distance_list)/point_len)*100
 print("-"*50)
 print("POSE ERROR:{}cm".format(pose_error))
 print("-"*50)
